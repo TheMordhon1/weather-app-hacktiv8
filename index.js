9264899ef538575c6562341969384379260aa709
themordhon <romdhonm12@gmail.com>
2024-07-04 23:03:28 +0700

    update: format clock

--- index.js
+++ index.js
@@ -191,8 +191,8 @@ const getDailyWeather = async (latitude, longitude) => {
 function updateClock() {
   const clock = document.getElementById("clock");
   const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
 
   const timeString = `${hours} : ${minutes}`;
 
