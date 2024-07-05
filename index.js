603c2d0a58519dbfff63033a40a3a8f4c4934a5d
themordhon <romdhonm12@gmail.com>
2024-07-05 23:48:30 +0700

    update: final

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const container = document.querySelector("#container");
 const userInput = document.querySelector("#search-input");
 const formInput = document.querySelector(".top-right-info");
 const city = document.querySelector("#location");
-const loading = document.querySelector("#loading");
 const dateTime = document.querySelector("#date-time");
+const loading = document.querySelector("#loading");
+let initialTimezone = "Asia/Jakarta";
 
 const weatherCodes = {
   0: "Langit cerah",
@@ -76,13 +77,20 @@ formInput.addEventListener("submit", async (event) => {
     );
     const rawData = await response.json();
     const data = rawData.results[0];
-    getCurrentWeather(data.latitude, data.longitude);
-    getDailyWeather(data.latitude, data.longitude);
+
+    getCurrentWeather(data.latitude, data.longitude, data.timezone);
+    getDailyWeather(data.latitude, data.longitude, data.timezone);
     userInput.value = "";
 
     // update city
     city.innerHTML =
       data.name == data.country ? data.name : `${data.name}, ${data.country}`;
+    const flag = document.createElement("img");
+    flag.src = `https://open-meteo.com//images/country-flags/${String(
+      data.country_code
+    ).toLowerCase()}.svg`;
+    flag.width = 30;
+    city.append(flag);
   } catch (error) {
     console.log("submit:", error);
     const errorText = document.getElementById("error-text");
@@ -120,18 +128,32 @@ const formattingDate = (dateData, selector, wTime) => {
   const month = months[dateString.getMonth()];
   const year = dateString.getFullYear();
 
-  const h = dateString.getHours();
-  const m = dateString.getMinutes();
+  const options = {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: initialTimezone,
+  };
+
+  const timeString = new Intl.DateTimeFormat("en-US", options).format(
+    dateString
+  );
 
   selector.innerHTML = wTime
-    ? `${day}, ${date} ${month} ${year} ${h}:${m} WIB`
-    : `${date} ${month} ${year}`;
+    ? `${day}, ${date} ${month} ${year} ${timeString}`
+    : `${day}, ${date} ${month} ${year}`;
 };
 
-const getCurrentWeather = async (latitude, longitude) => {
+const getCurrentWeather = async (
+  latitude,
+  longitude,
+  timezone = initialTimezone
+) => {
+  const timeZone = String(timezone).replace("/", "%2F");
   try {
     const response = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min&timezone=Asia%2FBangkok`
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min&timezone=${
+        timeZone || timezone
+      }`
     );
 
     const data = await response.json();
@@ -143,14 +165,14 @@ const getCurrentWeather = async (latitude, longitude) => {
       videoBackground.src = "./asset/day.mp4";
     }
 
-    formattingDate(data.current.time, dateTime, true);
+    formattingDate(data.current.time, dateTime, true, timezone);
+    updateClock(timezone);
 
     const currentWeatherIcon = document.querySelector("#current-weather-icon");
     currentWeatherIcon.src = `asset/weather/${
       weatherIcons[data.current.weather_code]
     }`;
 
-    // console.log(currentWeatherIcon);
     const weatherName = document.querySelector("#weather-name");
     weatherName.innerHTML = weatherCodes[data.current.weather_code];
 
@@ -170,10 +192,10 @@ const getCurrentWeather = async (latitude, longitude) => {
   }
 };
 
-const getDailyWeather = async (latitude, longitude) => {
+const getDailyWeather = async (latitude, longitude, timezone) => {
   try {
     const response = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Asia%2FBangkok`
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=${timezone}`
     );
     const data = await response.json();
 
@@ -223,12 +245,16 @@ const getDailyWeather = async (latitude, longitude) => {
 const getInitialWeatherInfo = () => {
   loading.style.display = "flex";
   container.style.display = "none";
-  getCurrentWeather("-6.4213", "106.7217");
-  getDailyWeather("-6.4213", "106.7217");
+  const flag = document.createElement("img");
+  flag.src = "https://open-meteo.com//images/country-flags/id.svg";
+  flag.width = 30;
+  getCurrentWeather("-6.4213", "106.7217", initialTimezone);
+  getDailyWeather("-6.4213", "106.7217", initialTimezone);
   setTimeout(() => {
     loading.style.display = "none";
     container.style.display = "flex";
     city.innerHTML = "Bogor, Indonesia";
+    city.append(flag);
   }, 2000);
 };
 
@@ -237,13 +263,14 @@ getInitialWeatherInfo();
 function updateClock() {
   const clock = document.getElementById("clock");
   const now = new Date();
-  const hours = String(now.getHours()).padStart(2, "0");
-  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const options = {
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: initialTimezone,
+  };
 
-  const timeString = `${hours} : ${minutes}`;
-
+  const timeString = new Intl.DateTimeFormat("en-US", options).format(now);
   clock.innerHTML = timeString;
 }
 
-updateClock();
 setInterval(updateClock, 1000);
